337fa4c20cf65541c9a7d57cfbf55221d88c100c
lyapin <lajpinkonstantin@gmail.com>
2025-09-16 22:35:16 +0300

    add pid worker

--- start.sh
+++ start.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+
 set -e
 
+# Активируем окружение Poetry
+source $(poetry env info --path)/bin/activate
+
+# Устанавливаем PYTHONPATH
+export PYTHONPATH="/home/laser_boy/switchman/src:$PYTHONPATH"
+
 # Загрузка токена
-export BOT_TOKEN=$(grep "BOT_TOKEN" .env | cut -d= -f2 -)
+export BOT_TOKEN=$(grep "BOT_TOKEN" .env | cut -d= -f2)
 echo "$BOT_TOKEN : BOT_TOKEN loaded"
 
-# Запуск через Poetry
-poetry run python -m src.main
+# Запускаем в режиме демона
+nohup python -m main > /dev/null 2>&1 &
 
-# Очистка
-unset BOT_TOKEN
+# Сохраняем PID процесса для возможности управления им later
+echo $! > bot.pid
+
+echo "Бот запущен в фоновом режиме с PID: $!"
