035cc680695fd0a53f878af5b422c4205aa3d23b
lyapin <lajpinkonstantin@gmail.com>
2025-11-30 00:10:14 +0300

    hook_setting

--- src/main.py
+++ src/main.py
@@ -80,6 +80,9 @@ async def setup():
     return app
 
 if __name__ == '__main__':
+    @bot.message_handler(func=lambda message: True)
+    async def echo_all(message):
+        await bot.send_message(message.chat.id, f"Вы сказали: {message.text}")
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     context.load_cert_chain(bot_settings.webhook_ssl_cert, bot_settings.webhook_ssl_priv)
     # Start aiohttp server
